e16e309964efa35c4ded3dcfec2e60a6bd885be0
Akhil <sahuv3694@gmail.com>
2026-08-13 16:00:50 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (16.33%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int length=1;
+        ListNode temp=head;
+        for(ListNode t=head; t.next!=null; t=t.next){
+            length++;
+        }
+        for(int i=1; i<=length/2; i++){
+            temp=temp.next;
+        }
+        return temp;
+    }
+}
